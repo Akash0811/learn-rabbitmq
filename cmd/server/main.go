a6c04b9465423625eaa58bd075717ee890653f38
bootdev <bootdev@DLP-I516-230.localdomain>
2026-05-23 13:26:52 +0000

    feat: add aqmp support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Application could not connect to RabbitMQ")
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ server")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Received CTRL+C...")
+	fmt.Println("Shutting Down...")
 }
