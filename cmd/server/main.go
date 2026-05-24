d8bf7c69381e0a94d99b286fea04af6900020fea
Akash Biswal <aakashbiswal@gmail.com>
2026-05-24 09:57:28 +0000

    feat: add publish message in server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,25 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Successfully connected to RabbitMQ server")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Failed to create channel")
+		return
+	}
+
+	err = pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Failed to publish JSON data due to %v\n", err)
+		return
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
